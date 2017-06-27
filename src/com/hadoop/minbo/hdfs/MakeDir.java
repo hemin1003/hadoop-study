2bd6139a5b8f68dd8e14115da636214994f99ff6
贺敏 <hemin_it@163.com>
2017-06-27 10:44:36 +0800

    update

--- src/com/hadoop/minbo/hdfs/MakeDir.java
+++ src/com/hadoop/minbo/hdfs/MakeDir.java
@@ -7,13 +7,16 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class MakeDir {
-	public static void main(String[] args) throws IOException {  
-        Configuration conf = new Configuration();  
-        FileSystem fs = FileSystem.get(conf);  
-        Path path = new Path("/user/local/hadoop/data/20130709");  
-        fs.create(path);  
-        fs.close();  
-        
-        System.out.println("done");
-    }  
+	public static void main(String[] args) throws IOException {
+		// 设置hadoop安装路径
+		System.setProperty("hadoop.home.dir", "F:\\hadoop\\hadoop-2.7.3");
+
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path("/user/local/hadoop/data/20130709");
+		fs.create(path);
+		fs.close();
+
+		System.out.println("done");
+	}
 }
--- src/com/hadoop/minbo/hdfs/WriteFile.java
+++ src/com/hadoop/minbo/hdfs/WriteFile.java
@@ -9,15 +9,18 @@ import org.apache.hadoop.fs.Path;
 public class WriteFile {
 
 	public static void main(String[] args) throws IOException {
+		// 设置hadoop安装路径
+		System.setProperty("hadoop.home.dir", "F:\\hadoop\\hadoop-2.7.3");
+
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
 		Path path = new Path("/user/hadoop/hm2.txt");
 		FSDataOutputStream out = fs.create(path);
-		
+
 		// out.writeUTF("da jia hao,cai shi zhen de hao!");
 		// out.writeChars("abcdefg");
 		out.write("minbo".getBytes());
-		
+
 		fs.close();
 
 		System.out.println("done");
